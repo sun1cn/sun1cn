14b64a03d3f469adcb89130c3fa98eef9c9f6059
sun1cn <104716517+sun1cn@users.noreply.github.com>
2022-05-01 18:08:55 +0800

    Add files via upload

---
+++ 2.12  编程练习第一题/2.12  编程练习第一题/第一题.cpp
@@ -0,0 +1,16 @@
+//笔上得来终觉浅
+//绝知此事要躬行
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("GUustav Mahler\n");
+	printf("Gustav\nMahler\n");
+	printf("Gustav");
+	printf("Mahler\n");
+
+	return 0;
+
+
+}
